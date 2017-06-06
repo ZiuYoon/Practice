329aee55b0bca883a0cbee0d45c37b33bc71fccd
윤서아 <yk9686@gmail.com>
2017-06-06 16:01:25 +0900

    `*inputable!!!!* `

---
+++ personal/off_chat/off_chat.js
@@ -0,0 +1,10 @@
+
+
+function sentenceUp(){
+    document.getElementById("box").innerHTML += (document.getElementById("input_box_detail").value+"<br/>");
+    /*var a = document.getElementById("box");
+    var t = document.getElementById("input_box");
+    a.innerHTML;
+    += (t.value+"<br/>");*/
+}
+
